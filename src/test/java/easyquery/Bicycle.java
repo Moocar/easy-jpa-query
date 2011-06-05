d95e5ebab1c32ef4353dfb830a0c846acb89ac05
Anthony Marcar <Anthony.Marcar@gmail.com>
2011-06-04 23:32:17 -0700

    more tests

--- src/test/java/easyquery/Bicycle.java
+++ src/test/java/easyquery/Bicycle.java
@@ -10,6 +10,8 @@ public class Bicycle {
     private int id;
     
     private String model;
+    
+    private long timestamp;
 
     @Id
     @GeneratedValue
@@ -27,5 +29,43 @@ public class Bicycle {
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Bicycle other = (Bicycle) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if ((this.model == null) ? (other.model != null) : !this.model.equals(other.model)) {
+            return false;
+        }
+        if (this.timestamp != other.timestamp) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + this.id;
+        hash = 37 * hash + (this.model != null ? this.model.hashCode() : 0);
+        hash = 37 * hash + (int) (this.timestamp ^ (this.timestamp >>> 32));
+        return hash;
     }
 }
--- src/test/java/easyquery/EasyJpaQueryTest.java
+++ src/test/java/easyquery/EasyJpaQueryTest.java
@@ -1,12 +1,17 @@
 package easyquery;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.Persistence;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
 
 public class EasyJpaQueryTest {
     
@@ -24,7 +29,7 @@ public class EasyJpaQueryTest {
     
     @Test
     public void testGetAll() {
-        createBicycle();
+        createBicycle("Peugot");
         
         ImmutableList<Bicycle> bicycles = easyQueryProvider
                 .select(Bicycle.class)
@@ -35,7 +40,7 @@ public class EasyJpaQueryTest {
 
     @Test
     public void testGetOneAttribute() {
-        createBicycle();
+        createBicycle("Peugot");
         
         String model = easyQueryProvider
                   .select(Bicycle_.model)
@@ -44,12 +49,111 @@ public class EasyJpaQueryTest {
         assertEquals("Peugot", model);
     }
 
-    private void createBicycle() {
+    @Test
+    public void testIn() {
+        createBicycle("Peugot1");
+        createBicycle("Peugot2");
+        createBicycle("Peugot3");
+        
+        ImmutableList<Bicycle> bicycles = easyQueryProvider
+                .select(Bicycle.class)
+                .where(Bicycle_.model).in(ImmutableSet.of("Peugot1", "Peugot3"))
+                .getResultList();
+
+        assertEquals(2, bicycles.size());
+    }
+
+    @Test
+    public void testLike() {
+        createBicycle("Peugot1");
+        createBicycle("Barft2");
+        createBicycle("Peugot3");
+        
+        ImmutableList<Bicycle> bicycles = easyQueryProvider
+                .select(Bicycle.class)
+                .where(Bicycle_.model).like("Peugot%")
+                .getResultList();
+
+        assertEquals(2, bicycles.size());
+    }
+
+    @Test
+    public void testSelectAttribute() {
+        createBicycle("i1");
+        createBicycle("i2");
+        createBicycle("i3");
+        
+        ImmutableList<String> bicycles = easyQueryProvider
+                .select(Bicycle_.model)
+                .getResultList();
+
+        assertTrue(ImmutableSet.of("i1", "i2", "i3").containsAll(bicycles));
+    }
+
+    @Test
+    public void testSingle() {
+        createBicycle("desc1");
+        createBicycle("desc2");
+        createBicycle("desc3");
+        
+        Bicycle bicycle = easyQueryProvider
+                .select(Bicycle.class)
+                .where(Bicycle_.model).equals("desc3")
+                .getSingleResult();
+
+        assertEquals("desc3", bicycle.getModel());
+
+        try {
+            easyQueryProvider.select(Bicycle.class)
+                    .getSingleResult();
+            fail();
+
+        } catch (NonUniqueResultException e) {
+            System.out.println(e.getClass() + ": " + e.getMessage());
+            // it's ok
+        }
+    }
+
+    @Test
+    public void testOrder() {
+        
+        long time = System.currentTimeMillis();
+        Bicycle bicycle1 = createBicycle("desc", time + 3);
+        Bicycle bicycle2 = createBicycle("desc", time + 1);
+        Bicycle bicycle3 = createBicycle("desc2", time + 2);
+        
+        ImmutableList<Bicycle> bicycles = easyQueryProvider
+                .select(Bicycle.class)
+                .orderBy(Bicycle_.timestamp).asc()
+                .getResultList();
+        
+        assertTrue(ImmutableList.of(bicycle2, bicycle3, bicycle1).containsAll(bicycles));
+        assertTrue(bicycles.equals(ImmutableList.of(bicycle2, bicycle3, bicycle1).asList()));
+        assertFalse(bicycles.equals(ImmutableList.of(bicycle1, bicycle2, bicycle3)));
+        
+        bicycles = easyQueryProvider
+                .select(Bicycle.class)
+                .where(Bicycle_.model).equals("desc")
+                .orderBy(Bicycle_.timestamp).asc()
+                .getResultList();
+
+        assertTrue(ImmutableList.of(bicycle2, bicycle1).containsAll(bicycles));
+        assertTrue(bicycles.equals(ImmutableList.of(bicycle2, bicycle1).asList()));
+        assertFalse(bicycles.equals(ImmutableList.of(bicycle1, bicycle2)));
+    }
+
+    private Bicycle createBicycle(String model) {
+        return createBicycle(model, System.currentTimeMillis());
+    }
+
+    private Bicycle createBicycle(String model, long timestamp) {
         beginTx();
         Bicycle bicycle = new Bicycle();
-        bicycle.setModel("Peugot");
+        bicycle.setModel(model);
+        bicycle.setTimestamp(timestamp);
         entityManager.persist(bicycle);
         commitTx();
+        return bicycle;
     }
 
     private void beginTx() {
